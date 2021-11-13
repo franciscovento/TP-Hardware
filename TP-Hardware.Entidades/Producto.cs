ce7eb29c66b3520f6972de6ccd6cdd2e5a0ba60e
Garra <70707057+GonzaloErra@users.noreply.github.com>
2021-11-12 22:05:01 -0300

    Commit 1 (Agregado de Entidades)

--- TP-Hardware.Entidades/Producto.cs
+++ TP-Hardware.Entidades/Producto.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace TP_Hardware.Entidades
 {
-    class Producto
+    public abstract class Producto
     {
+        private int idCategoria;
+        private string nombre;
+        private int stock;
+        private int proveedor;
+        private int precio;
+        private DateTime fechaAlta;
+        private int usuario;
+        private int id;
+
+        protected Producto()
+        {
+        }
+
+        protected Producto(int idCategoria, string nombre, int stock, int proveedor, int precio, DateTime fechaAlta, int usuario, int id)
+        {
+            IdCategoria = idCategoria;
+            Nombre = nombre;
+            Stock = stock;
+            Proveedor = proveedor;
+            Precio = precio;
+            FechaAlta = fechaAlta;
+            Usuario = usuario;
+            Id = id;
+        }
+        protected int Id { get => id; set => id = value; }
+        protected int IdCategoria { get => idCategoria; set => idCategoria = value; }
+        protected string Nombre { get => nombre; set => nombre = value; }
+        protected int Stock { get => stock; set => stock = value; }
+        protected int Proveedor { get => proveedor; set => proveedor = value; }
+        protected int Precio { get => precio; set => precio = value; }
+        protected DateTime FechaAlta { get => fechaAlta; set => fechaAlta = value; }
+        protected int Usuario { get => usuario; set => usuario = value; }
     }
 }
